fc82755b1f33db25ec627d3ce86b18e660f51011
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-08 14:27:02 +0100

    Refactors Jazzbuzz to take number & divisor args

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,11 +2,14 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz._isDivisibleBy(3, 3)).toBe(true);
     });
 
   });
@@ -14,8 +17,39 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+      expect(javabuzz._isDivisibleBy(2, 3)).toBe(false);
+    });
+
+  });
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 5', function() {
+      expect(javabuzz._isDivisibleBy(10, 5)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by 5', function() {
+      expect(javabuzz._isDivisibleBy(7, 5)).toBe(false);
+    });
+
+  });
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 5', function() {
+      expect(javabuzz._isDivisibleBy(30, 15)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by 5', function() {
+      expect(javabuzz._isDivisibleBy(4, 15)).toBe(false);
     });
 
   });
